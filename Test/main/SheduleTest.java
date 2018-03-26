a80f1dfd0eeac7b4c46a562fa62930aa9299516b
ADsty <32031947+ADsty@users.noreply.github.com>
2018-03-26 17:39:58 +0300

    Additions to classes

--- Test/main/SheduleTest.java
+++ Test/main/SheduleTest.java
@@ -23,13 +23,13 @@ public class SheduleTest {
         forComparison = new ArrayList<>();
         testFirst = new Train("lastochka", "Moscow", LocalTime.of(8, 0, 0, 0));
         testSecond = new Train("sokol", "NY", LocalTime.of(9, 0, 0, 0));
+        t.addNewTrain(testFirst);
+        t.addNewTrain(testSecond);
     }
 
     @Test
     public void findTrain() {
         Train testThird = new Train("T600", "Moscow", LocalTime.of(7, 47, 0, 0));
-        t.addNewTrain(testFirst);
-        t.addNewTrain(testSecond);
         t.addNewTrain(testThird);
         forComparison.add(testThird);
         assertEquals(forComparison, t.findTrain("Moscow", LocalTime.of(7, 45, 0, 0)));
@@ -37,35 +37,28 @@ public class SheduleTest {
 
     @Test
     public void findWrongTrain() {
-        t.addNewTrain(testFirst);
-        t.addNewTrain(testSecond);
         assertEquals(forComparison, t.findTrain("Kirishi", LocalTime.of(7, 45, 0, 0)));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void wrongDataFindTrain() {
-        t.addNewTrain(testFirst);
         t.findTrain("/*/*//=-=", LocalTime.of(7, 45, 0, 0));
     }
 
     @Test
     public void removeTrain() {
-        t.addNewTrain(testFirst);
-        t.addNewTrain(testSecond);
         t.removeTrain("sokol");
         assertEquals(forComparison, t.findTrain("NY", LocalTime.of(7, 45, 0, 0)));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void wrongDataRemoveTrain() {
-        t.addNewTrain(testFirst);
         t.removeTrain("*/*/*//-=-=");
     }
 
     @Test
     public void addNewTrain() {
         testFirst.addIntStation("SPB");
-        t.addNewTrain(testFirst);
         forComparison.add(testFirst);
         assertEquals(forComparison, t.findTrain("Moscow", LocalTime.of(7, 45, 0, 0)));
     }
